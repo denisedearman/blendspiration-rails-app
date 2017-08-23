26b5d640dcc613f5694d91df549e3f68afbb6fe6
denisedearman <dearmandenise@gmail.com>
2017-08-23 16:07:25 -0400

    fixed the routes for the callbacks controller and removed sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root 'welcome#home'
-  get '/about' => 'welcome#about'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 end
