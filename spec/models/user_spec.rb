8c80e98fc938aa398b51d9e722449da62d34e08b
denisedearman <dearmandenise@gmail.com>
2017-08-24 09:52:40 -0400

    added tests for users model and added fake seed for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.first
+  end
+
+  it "has many recipes" do
+    @user.recipes.create
+    expect(@user.recipes.count).to eq(1)
+  end
+
 end
